51e57d3acb248de377a6b1bfbe1d73ff4337c5cb
tonmoyprogrammer <debtonmoy583@gmail.com>
2026-01-02 01:13:35 +0600

    This is my card done commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-
+import { Suspense } from 'react';
 import './App.css'
 import AvailablePlayers from './components/availablePlayers/availablePlayers';
 import NavBar from './components/NavBar/navBar';
 
+const fetchPlayers = async()=>
+{
+  const res = await fetch("/players.json")
+  return res.json();
+}
+
 function App() {
  
+  const getFetchPlayers = fetchPlayers();
   return (
     <>
    
    <NavBar></NavBar>
-   <AvailablePlayers></AvailablePlayers>
+   <Suspense fallback={<span className="loading loading-dots loading-md"></span>}>
+    <AvailablePlayers getPlayers={getFetchPlayers}></AvailablePlayers>
+   </Suspense>
 
     </>
   )
