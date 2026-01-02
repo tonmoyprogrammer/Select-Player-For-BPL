ba7b928e2c12579830c2f4783fd64b2608f1a093
tonmoyprogrammer <debtonmoy583@gmail.com>
2026-01-03 01:29:54 +0600

    This is another commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { Suspense } from 'react';
+import { Suspense, useState } from 'react';
 import './App.css'
 import AvailablePlayers from './components/availablePlayers/availablePlayers';
 import NavBar from './components/NavBar/navBar';
+import SelectedPlayers from './components/SelectedPLayers/selectedPlayers';
 
 const fetchPlayers = async()=>
 {
@@ -10,15 +11,35 @@ const fetchPlayers = async()=>
 }
 
 function App() {
+  const[toggle,setToggle] = useState(true);
  
   const getFetchPlayers = fetchPlayers();
   return (
     <>
    
    <NavBar></NavBar>
-   <Suspense fallback={<span className="loading loading-dots loading-md"></span>}>
+   <div className="max-w-[1200px] mx-auto border-2 border-red-300
+    flex justify-between items-center">
+    <h1 className="font-bold text-2xl">Avaiable</h1>
+
+    <div className="font-bold">
+      <button onClick={()=>setToggle(true)} className={`py-3 px-4 border-1 border-r-0
+       border-gray-400
+       rounded-l-2xl   ${toggle == true ? "bg-[#E7FE29]":""}`}>Avaiable</button>
+      <button onClick={()=>setToggle(false)} className={`py-3 px-4 border-1 rounded-r-2xl rounded-l-0
+       border-gray-400 border-l-0  ${toggle == false ? "bg-[#E7FE29]":""}`}>Selected <span>(0)</span></button>
+    </div>
+   </div>
+   {
+     toggle == true? <Suspense fallback={<span className="loading loading-dots loading-md"></span>}>
     <AvailablePlayers getPlayers={getFetchPlayers}></AvailablePlayers>
-   </Suspense>
+   </Suspense>:   <SelectedPlayers></SelectedPlayers>
+   }
+
+   
+
+
+
 
     </>
   )
--- src/components/SelectedPLayers/selectedPlayers.jsx
+++ src/components/SelectedPLayers/selectedPlayers.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 const SelectedPlayers = () => {
     return (
-        <div>
+        <div className='max-w-[1200px] mx-auto'>
+            i am selected
             
         </div>
     );
